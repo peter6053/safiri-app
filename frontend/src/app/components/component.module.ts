1a89cb875e488b60235d4c53221db8fc2d22fefc
Asante Julius Kyere <asantejuliuskyere@gmail.com>
2021-09-24 20:14:59 +0000

    added footer

--- frontend/src/app/components/component.module.ts
+++ frontend/src/app/components/component.module.ts
@@ -1,16 +1,19 @@
 import { HeaderComponent } from './header/header.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ]
 })
 
