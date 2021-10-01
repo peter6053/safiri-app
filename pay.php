3ea51a58949a1d5994b75f7e8778abb9a406dfb2
peter6053 <35918656+peter6053@users.noreply.github.com>
2021-10-01 03:55:32 -0500

    Add files via upload

---
+++ pay.php
@@ -0,0 +1,62 @@
+<?php 
+if(isset($_POST['pay']))
+{
+    $email = $_POST['email'];
+    $amount = $_POST['amount'];
+
+    //* Prepare our rave request
+    $request = [
+        'tx_ref' => time(),
+        'amount' => $amount,
+        'currency' => 'NGN',
+        'payment_options' => 'card',
+        'redirect_url' => 'http://localhost/yt/rave/process.php',  //we will need to redirect it the the webserver
+        'customer' => [                                              
+            'email' => $email,
+            'name' => 'Zubdev'
+        ],
+        'meta' => [
+            'price' => $amount
+        ],
+        'customizations' => [
+            'title' => 'kindly pay for your destination',
+            'description' => 'we hope you enjoy your journey'
+        ]
+    ];
+
+    //* Ca;; f;iterwave emdpoint
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://api.ravepay.co/flwv3-pug/getpaidx/api/v2/hosted/pay",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS => json_encode($request),
+    CURLOPT_HTTPHEADER => array(
+        'Authorization: Bearer FLWSECK_TEST-eee25be1b44ef9a132a872075b3a0910-X',
+        'Content-Type: application/json'
+    ),
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+    
+    $res = json_decode($response);
+    if($res->status == 'success')
+    {
+        $link = $res->data->link;
+        header('Location: '.$link);
+    }
+    else
+    {
+        echo 'We can not process your payment';
+    }
+}
+
+?>
